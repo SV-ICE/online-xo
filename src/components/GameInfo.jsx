23ef3549b05a00a0eaf306ac88110143564b0120
SV-ICE <hristyuk.sv@gmail.com>
2025-11-08 10:43:04 +0200

    ✨ update(ui): обновить стили и логику отображения статуса
    Обновлены стили для компонента game-status, теперь используется grid layout для более гибкого отображения информации о ходе игры и победителе. Добавлена анимация для отображения текущего хода и победителя с использованием библиотеки framer-motion. Добавлены стили для обнуления margin и padding у всех элементов.

--- src/components/GameInfo.jsx
+++ src/components/GameInfo.jsx
@@ -1,11 +1,12 @@
 import GameSymbol from "./GameSymbol";
+import { motion, AnimatePresence } from "framer-motion";
 import { symbolVariants } from "./../constants";
 
 const GameInfo = ({ isDraw, winnerSymbol, currentStep }) => {
     if (isDraw) {
         return (
             <div className="game-info">
-                <span className="game-status">Ничья</span>
+                <span className="game-status status-result">Ничья</span>
             </div>
         );
     }
@@ -13,8 +14,24 @@ const GameInfo = ({ isDraw, winnerSymbol, currentStep }) => {
     if (winnerSymbol) {
         return (
             <div className="game-info">
-                <span className="game-status">
-                    Победитель: <GameSymbol symbol={winnerSymbol} />
+                <span className="game-status status-result">
+                    <p>Победитель:</p>
+                    <AnimatePresence>
+                        <motion.span
+                            key={winnerSymbol}
+                            variants={symbolVariants}
+                            initial="hidden"
+                            animate="visible"
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                width: "40px",
+                                height: "100%",
+                            }}>
+                            <GameSymbol symbol={winnerSymbol} />
+                        </motion.span>
+                    </AnimatePresence>
                 </span>
             </div>
         );
@@ -23,7 +40,23 @@ const GameInfo = ({ isDraw, winnerSymbol, currentStep }) => {
     return (
         <div className="game-info">
             <span className="game-status">
-                Ход: <GameSymbol symbol={currentStep} />
+                <p>Ход:</p>
+                <AnimatePresence>
+                    <motion.span
+                        key={currentStep}
+                        variants={symbolVariants}
+                        initial="hidden"
+                        animate="visible"
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            width: "40px",
+                            height: "100%",
+                        }}>
+                        <GameSymbol symbol={currentStep} />
+                    </motion.span>
+                </AnimatePresence>
             </span>
         </div>
     );
